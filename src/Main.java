af9eb9a647abb65ae0942153a92eec642b56e4ac
ReconIMSF <dorbatorg@gmail.com>
2024-11-08 00:49:55 +0800

    Homework 8 done

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,74 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Задание 1
+        System.out.println("Задание 1");
+        int [] A = new int[]{1, 2, 3};
+        double [] B = {1.57, 7.654, 9.986};
+        int [] C = {7, 8, 9};
+
+        // Задание 2
+        System.out.println("Задание 2");
+        for (int i = 0; i < A.length; i++) {
+            if (i <= 1) {
+                System.out.print(A[i] + ", ");
+            } else {
+                System.out.print(A[i]);
+            }
+        }
+        System.out.println(" ");
+        for (int i = 0; i < B.length; i++) {
+            if (i <= 1) {
+                System.out.print(B[i] + ", ");
+            } else {
+                System.out.print(B[i]);
+            }
+        }
+        System.out.println(" ");
+        for (int i = 0; i < C.length; i++) {
+            if (i <= 1) {
+                System.out.print(C[i] + ", ");
+            } else {
+                System.out.print(C[i]);
+            }
+        }
+        System.out.println(" ");
+
+        // Задание 3
+        System.out.println("Задание 3");
+        for (int i = 2; i < A.length; i--) {
+            if (i >= 1) {
+                System.out.print(A[i] + ", ");
+            } else if (i == 0) {
+                System.out.print(A[i]);
+            }
+        }
+        System.out.println(" ");
+        for (int i = 2; i < B.length; i--) {
+            if (i >= 1) {
+                System.out.print(B[i] + ", ");
+            } else if (i == 0) {
+                System.out.print(B[i]);
+            }
+        }
+        System.out.println(" ");
+        for (int i = 2; i < C.length; i--) {
+            if (i >= 1) {
+                System.out.print(C[i] + ", ");
+            } else if (i == 0) {
+                System.out.print(C[i]);
+            }
+        }
+        System.out.println(" ");
+        // Задание 4
+        System.out.println("Задание 4");
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 != 0) {
+                A[i] = A[i] + 1;
+            }
+            System.out.println(A[i]);
         }
     }
 }
